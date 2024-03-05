b09669f47b08d5125c182be6892936046292dd23
ikromshi <numanovikrom454@gmail.com>
2024-03-05 16:22:41 -0500

    sprint boot devtools intro

---
+++ app/src/main/java/com/ikromshi/springboot/demo/app/rest/FunRestController.java
@@ -0,0 +1,28 @@
+package com.ikromshi.springboot.demo.app.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    // expose a "/" endpoint that returns a "Hello World."
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello bitches";
+    }
+
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5K";
+    }
+
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day";
+    }
+}
