6489a81e5262d9890d5261f940bd14bc5489160f
ikromshi <numanovikrom454@gmail.com>
2024-03-17 18:49:16 -0400

    application.properties injection

--- app/src/main/java/com/ikromshi/springboot/demo/app/rest/FunRestController.java
+++ app/src/main/java/com/ikromshi/springboot/demo/app/rest/FunRestController.java
@@ -1,11 +1,26 @@
 package com.ikromshi.springboot.demo.app.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
     
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose a new endpoint for "teamInfo"
+    @GetMapping("/teamInfo")
+    public String teamInfo() {
+        return "Coach Name: " + coachName + ". Team Name: " + teamName;
+    }
+
+
     // expose a "/" endpoint that returns a "Hello World."
     @GetMapping
     public String sayHello() {
