e048f0b01f9a2a22e56bcda81db1742d15f082c5
ikromshi <numanovikrom454@gmail.com>
2024-04-14 16:15:54 -0400

    qualifier example complete

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/ikromshi/springcoredemo/rest/Controller.java
@@ -0,0 +1,25 @@
+package com.ikromshi.springcoredemo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ikromshi.springcoredemo.common.Coach;
+
+@RestController
+public class Controller {
+    // define a  private field for the dependency;
+    private Coach myCoach;
+
+    // create a constructor in the class for dependency injection
+    @Autowired
+    public Controller(@Qualifier("trackCoach") Coach theCoach) {
+        this.myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout() {
+        return this.myCoach.getDailyWorkout();
+    }
+}
