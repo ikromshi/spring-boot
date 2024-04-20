a61c62c3bdbdb090c0234e9e01269d310a8eb202
ikromshi <numanovikrom454@gmail.com>
2024-04-20 15:59:06 -0400

    saving multiple objects in the db

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/ikromshi/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/ikromshi/cruddemo/CruddemoApplication.java
@@ -18,14 +18,15 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) { // spring will inject the bean; diff between repo, bean, and component
 		return runner -> {
-			createStudent(studentDAO);
+			// createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
 		// create a student
 		System.out.println("Creating a new student object...");
-		Student tempStudent = new Student("Ikrom", "", "email@ikromshi.com");
+		Student tempStudent = new Student("Ikrom", "Numonov", "email@ikromshi.com");
 
 		// save the student object
 		System.out.println("Saving the student...");
@@ -33,5 +34,19 @@ public class CruddemoApplication {
 
 		// display ID of the saved student
 		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		// create students
+		System.out.println("Creating three student objects...");
+		Student student1 = new Student("Ikrom", "Numonov", "email@ikromshi.com");
+		Student student2 = new Student("John", "Doe", "john@doe@email.com");
+		Student student3 = new Student("Mary", "Public", "mary@email.com");
+		
+		// save the students
+		System.out.println("Saving the students...");
+		studentDAO.save(student1);
+		studentDAO.save(student2);
+		studentDAO.save(student3);
 	}
 }
