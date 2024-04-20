c65f715fd3740621fb4f97cb1bc7078004f94abe
ikromshi <numanovikrom454@gmail.com>
2024-04-20 15:53:18 -0400

    completely saving an object in the db

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/ikromshi/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/ikromshi/cruddemo/CruddemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.ikromshi.cruddemo.dao.StudentDAO;
+import com.ikromshi.cruddemo.entity.Student;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -13,9 +16,22 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) { // spring will inject the bean; diff between repo, bean, and component
 		return runner -> {
-			System.out.println("hello world");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create a student
+		System.out.println("Creating a new student object...");
+		Student tempStudent = new Student("Ikrom", "", "email@ikromshi.com");
+
+		// save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		// display ID of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 }
